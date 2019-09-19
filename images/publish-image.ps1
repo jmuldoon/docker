5d64aab75494e123462f3306295d49ab83c2ced3
James P Muldoon <james@muldoon.io>
2019-09-20 00:28:24 +0100

    add badge and remove ps1

--- images/publish-image.ps1
+++
@@ -1,8 +0,0 @@
-param (
-  [Parameter(Mandatory=$true)][string]$IMAGE_PATH,
-  [string]$OWNER='jmuldoon'
-)
-
-$version = Get-Content $(Join-Path -Path "${IMAGE_PATH}" -ChildPath VERSION)
-docker build -t docker.pkg.github.com/${OWNER}/docker/${IMAGE_PATH}-ci:$version "${IMAGE_PATH}"
-docker push docker.pkg.github.com/${OWNER}/docker/${IMAGE_PATH}-ci:$version
